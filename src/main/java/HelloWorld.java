e2480b648d87be9ae8426b9731b84277074574f3
codevuive85 <sgl3338888@gmail.com>
2022-10-27 22:45:25 +0700

    sualoi cicd

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "luongit Hello world");
+		get("/hello", (req, res) -> "luongit 2 Hello world");
 	}
 }
