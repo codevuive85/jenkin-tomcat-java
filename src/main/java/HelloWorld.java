7989f180632ba7567c5520260bcf42bdc97d8cdf
codevuive85 <sgl3338888@gmail.com>
2022-10-29 17:19:28 +0700

    cap nhat lan 1

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Tomcat cicd");
+		get("/hello", (req, res) -> "Tomcat cicd Hello-World 1");
 	}
 }
